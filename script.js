2bada587715d4afef0cb5ef5dddef49061932a99
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-05-10 00:03:10 +0200

    conversion en plus
    conversion en plus

--- script.js
+++ script.js
@@ -140,6 +140,55 @@ document.addEventListener("DOMContentLoaded", function(event) {
       document.querySelector('#afichage_dx2').innerHTML = dx2;
     };
 
+
+    /* conversion */
+    const bouton_conversion = document.querySelector('#bouton_conversion');
+    bouton_conversion.onclick = function(){
+      var input_conversion = document.querySelector('#conversion');
+      conversion = input_conversion.value;
+      var output = 0;
+      for (var i = 0; i < conversion.length; i++) {
+        var actuelle = conversion[i].charCodeAt(0);
+        if(actuelle.toString().length == 1){
+          console.log("leng 1");
+          output += "00"+actuelle;
+        }else if (actuelle.toString().length == 2){
+          console.log("leng 2");
+          output += "0"+actuelle;
+        }else{
+          output += actuelle;
+        }
+        
+        
+      }
+      output = output.substring(1);
+      document.querySelector('#afichage_conversion').innerHTML = output;
+    };
+
+    /* conversion2 */
+    const bouton_conversion2 = document.querySelector('#bouton_conversion2');
+    bouton_conversion2.onclick = function(){
+      var input_conversion2 = document.querySelector('#conversion2');
+      conversion2 = input_conversion2.value;
+      console.log('conversion2: ', conversion2);
+      var output2 = 0;
+      var flag = 0;
+      var temp = "";
+      for (var i = 0; i < conversion2.length; i++) {   
+        flag++;
+        temp = temp + conversion2[i];
+        if (flag == 3){
+          flag = 0;
+          output2 = output2 + String.fromCharCode(parseInt(temp))
+          console.log('parseInt(temp): ', parseInt(temp));
+          temp = "";
+        }
+      }
+      output2 = output2.substring(1);
+      document.querySelector('#afichage_conversion2').innerHTML = output2;
+    };
+
+
     /* var de test p = 5 q = 17 e = 5   */
     var p = 0;
     var q = 0;
