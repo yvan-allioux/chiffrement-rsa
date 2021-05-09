b5d1ce0e8523b78e9b2139b414fecf989c53b562
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-05-09 21:54:56 +0200

    créa clé et chiff
    création de clé et chiffrement ok

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     button_n.onclick = function(){
       n = p*q; 
       document.querySelector('#afichage_n').innerHTML = n;
+      document.querySelector('#afichage_n2').innerHTML = n;
       fiN = (p - 1)*(q - 1);
       document.querySelector('#afichage_fiN').innerHTML = fiN;
     };
@@ -70,23 +71,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
       var input_e = document.querySelector('#e');
       e = parseInt(input_e.value);
       document.querySelector('#afichage_e').innerHTML = e;
+      document.querySelector('#afichage_e2').innerHTML = e;
       console.log('e: ', e); 
+      document.querySelector('#afichage_pgcden').innerHTML = pgcd(e,fiN);
+      d = euclide2(e,fiN);
+      document.querySelector('#afichage_d').innerHTML = d;
+      document.querySelector('#afichage_d2').innerHTML = d;
+    };
+    /* calcul de m avec le formulaire */
+    const button_m = document.querySelector('#bouton_m');
+    button_m.onclick = function(){
+      var input_m = document.querySelector('#m');
+      m = parseInt(input_m.value);
+      document.querySelector('#afichage_m').innerHTML = m;
+      x = Math.pow(m, e) % n;
+      document.querySelector('#afichage_x').innerHTML = x;
     };
 
-    var p = 5;
-    console.log('p: ', p);
-    var q = 17;
-    console.log('q: ', q);
-    var n = p*q; //85
-    console.log('n: ', n);
-    var fiN = (p - 1)*(q - 1); //64
-    console.log('fiN: ', fiN);
-    var e = 5;
-    console.log('e: ', e);
-    console.log("pgcd de e et N :"+pgcd(e,fiN)+ ", si c'est 1 c'est ok pour le e");
-    console.log("algo de euclide etendu :");
-    console.log("resultas euclide 2 :" + euclide2(e,fiN));
-    var d = euclide2(e,fiN);
-    console.log("clé publique: "+ n + " " + e);
-    console.log("clé privée :"+ d);
+    /* var de test p = 5 q = 17 e = 5   */
+    var p = 0;
+    var q = 0;
+    var n = 0;
+    var fiN = 0;
+    var e = 0;
+    var d = 0;
+    var m = 0;
+    var x = 0;
+
 });
